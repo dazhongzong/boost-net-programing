e82ae48c6b815597a6fac0c4165a504bb0572f83
lsz <1742428999@qq.com>
2024-04-11 18:08:21 +0800

    'concurrent'

--- Concurrent/day07-Async_Function/Async_Function.cpp
+++ Concurrent/day07-Async_Function/Async_Function.cpp
@@ -1,8 +1,9 @@
-﻿
+﻿#include <memory>
 #include <algorithm>
 #include <iostream>
 #include <list>
-
+#include "thread_pool.h"
+#include <array>
 //c++ 版本的快速排序算法
 template<typename T>
 void quick_sort_recursive(T arr[], int start, int end) {
@@ -69,10 +70,85 @@ std::list<T> sequential_quick_sort(std::list<T> input)
     result.splice(result.begin(), new_lower);
     return result;
 }
+void test_sequential_quick() {
+    std::list<int> numlists = { 6,1,0,7,5,2,9,-1 };
+    auto sort_result = sequential_quick_sort(numlists);
+    std::cout << "sorted result is ";
+    for (auto iter = sort_result.begin(); iter != sort_result.end(); iter++) {
+        std::cout << " " << (*iter);
+    }
+    std::cout << std::endl;
+}
+
+//并行版本
+template<typename T>
+std::list<T> parallel_quick_sort(std::list<T> input)
+{
+    if (input.empty())
+    {
+        return input;
+    }
+    std::list<T> result;
+    result.splice(result.begin(), input, input.begin());
+    T const& pivot = *result.begin();
+    auto divide_point = std::partition(input.begin(), input.end(),
+        [&](T const& t) {return t < pivot; });
+    std::list<T> lower_part;
+    lower_part.splice(lower_part.end(), input, input.begin(),
+        divide_point);
+    // ①因为lower_part是副本，所以并行操作不会引发逻辑错误，这里可以启动future做排序
+    std::future<std::list<T>> new_lower(
+        std::async(&parallel_quick_sort<T>, std::move(lower_part)));
+
+    // ②
+    auto new_higher(
+        parallel_quick_sort(std::move(input)));
+    result.splice(result.end(), new_higher);
+    result.splice(result.begin(), new_lower.get());
+    return result;
+}
+
+
+//线程池版本
+//并行版本
+template<typename T>
+std::list<T> thread_pool_quick_sort(std::list<T> input)
+{
+    if (input.empty())
+    {
+        return input;
+    }
+    std::list<T> result;
+    result.splice(result.begin(), input, input.begin());
+    T const& pivot = *result.begin();
+    auto divide_point = std::partition(input.begin(), input.end(),
+        [&](T const& t) {return t < pivot; });
+    std::list<T> lower_part;
+    lower_part.splice(lower_part.end(), input, input.begin(),
+        divide_point);
+    // ①因为lower_part是副本，所以并行操作不会引发逻辑错误，这里投递给线程池处理
+    auto new_lower = ThreadPool::instance().commit(&parallel_quick_sort<T>, std::move(lower_part));
+    // ②
+    auto new_higher(
+        parallel_quick_sort(std::move(input)));
+    result.splice(result.end(), new_higher);
+    result.splice(result.begin(), new_lower.get());
+    return result;
+}
+void test_thread_pool_sort() {
+    std::list<int> numlists = { 6,1,0,7,5,2,9,-1 };
+    auto sort_result = thread_pool_quick_sort(numlists);
+    std::cout << "sorted result is ";
+    for (auto iter = sort_result.begin(); iter != sort_result.end(); iter++) {
+        std::cout << " " << (*iter);
+    }
+    std::cout << std::endl;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+      
+    return 0;
 }
 
 
